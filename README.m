11d95b736006e5d0e65e2a94c8cc67e1dc2043c8
Sahdat hossen <sahdathossen76@gmail.com>
2021-06-18 14:47:54 +0600

    food webside

--- README.m
+++ README.m
@@ -1,1 +1,7 @@
 # food_webside
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sahdathossen76/food_webside.git
+git push -u origin main
